479db8e9b534a8946b5d8b1c556fcb93ddc26d11
jrobinso <933148+jrobinso@users.noreply.github.com>
2023-03-09 14:29:31 -0800

    remove bgzip and google-utils dependencies

--- scripts/updateBGZip.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-
-const files = ['bgzip.js', 'pako.exm.js']
-for(let fn of files) {
-    const inputPath = `node_modules/bgzip/src/${fn}`
-    const outputPath = `src/${fn}`
-    const contents = fs.readFileSync(inputPath)
-    fs.writeFileSync(outputPath, contents)
-}
--- scripts/updateGoogleUtils.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-
-const googleFiles = ['googleAuth.js', 'googleDrive.js', 'googleUtils.js, googlePicker.js']
-for(let fn of googleFiles) {
-    const inputPath = `node_modules/google-utils/src/${fn}`
-    const outputPath = `src/${fn}`
-    const contents = fs.readFileSync(inputPath)
-    fs.writeFileSync(outputPath, contents)
-}
